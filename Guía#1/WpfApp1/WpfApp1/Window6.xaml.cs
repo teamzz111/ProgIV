bee8b17ca5639022caad038c10f58e2d52496094
teamzz111 <felipeflogxd@gmail.com>
2019-05-06 15:23:49 -0500

    Guía #1

---
+++ Guía#1/WpfApp1/WpfApp1/Window6.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Window6.xaml
+    /// </summary>
+    public partial class Window6 : Window
+    {
+        public Window6()
+        {
+            InitializeComponent();
+            cmbColors.ItemsSource = typeof(Colors).GetProperties();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String cad = cmbColors.SelectedItem.ToString();
+            String ControlString = cad.Split(' ')[1].TrimStart(' ');
+            MessageBox.Show(ControlString);
+        }
+
+      
+    }
+}
---
+++ Guía#1/WpfApp1/WpfApp1/Window7.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Window7.xaml
+    /// </summary>
+    public partial class Window7 : Window
+    {
+        public Window7()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String cad = combobox.SelectedItem.ToString();
+            String cs = cad.Split(' ')[1].TrimStart(' ');
+            MessageBox.Show(cs);
+        }
+    }
+}
